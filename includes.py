d1fd766333d746e76108f025f3cf2febf4cf1564
Jon White <white.jon@gmail.com>
2023-07-08 12:39:46 +0100

    Move the Column and attack label definitions to a separate file

---
+++ includes.py
@@ -0,0 +1,69 @@
+"""
+This module defines the global variables and dictionaries used in the project.
+
+It contains definitions for columns used in the dataset, mapping functions for 
+the attack labels, and several dictionaries of attack types for varying classes 
+of traffic.
+
+The dictionaries of attack types include:
+- 34 classes, which includes 33 attack classes and one for benign traffic
+- 8 classes, which includes separate classes for DDoS and DoS attacks
+- 7 classes, which groups DDoS and DoS attacks into a single class
+- 2 classes, which classifies traffic as either benign or malicious
+"""
+
+# Columns used in the dataset
+X_columns = [
+    'flow_duration', 'Header_Length', 'Protocol Type', 'Duration',
+    'Rate', 'Srate', 'Drate', 'fin_flag_number', 'syn_flag_number',
+    'rst_flag_number', 'psh_flag_number', 'ack_flag_number',
+    'ece_flag_number', 'cwr_flag_number', 'ack_count',
+    'syn_count', 'fin_count', 'urg_count', 'rst_count', 
+    'HTTP', 'HTTPS', 'DNS', 'Telnet', 'SMTP', 'SSH', 'IRC', 'TCP',
+    'UDP', 'DHCP', 'ARP', 'ICMP', 'IPv', 'LLC', 'Tot sum', 'Min',
+    'Max', 'AVG', 'Std', 'Tot size', 'IAT', 'Number', 'Magnitue',
+    'Radius', 'Covariance', 'Variance', 'Weight', 
+]
+
+# DF column used for the attack labels
+y_column = 'label'
+
+# Mapping functions for the attack labels
+# Creating a dictionary of attack types for 33 attack classes + 1 for benign traffic
+dict_34_classes = {'BenignTraffic': 0 ,                                                                                                                         # Benign
+                    'DDoS-RSTFINFlood' :1, 'DDoS-PSHACK_Flood':2,  'DDoS-SYN_Flood':3, 'DDoS-UDP_Flood':4, 'DDoS-TCP_Flood':5, 
+                    'DDoS-ICMP_Flood':6, 'DDoS-SynonymousIP_Flood':7, 'DDoS-ACK_Fragmentation':8, 'DDoS-UDP_Fragmentation':9, 'DDoS-ICMP_Fragmentation':10, 
+                    'DDoS-SlowLoris':11, 'DDoS-HTTP_Flood':12, 'DoS-UDP_Flood':13, 'DoS-SYN_Flood':14, 'DoS-TCP_Flood':15, 'DoS-HTTP_Flood':16,                 # DDoS and DoS
+                    'Mirai-greeth_flood': 17, 'Mirai-greip_flood': 18, 'Mirai-udpplain': 19,                                                                    # Mirai 
+                    'Recon-PingSweep': 20, 'Recon-OSScan': 21, 'Recon-PortScan': 22, 'VulnerabilityScan': 23, 'Recon-HostDiscovery': 24,                        # Reconnaissance
+                    'DNS_Spoofing': 25, 'MITM-ArpSpoofing': 26,                                                                                                 # Spoofing
+                    'BrowserHijacking': 27, 'Backdoor_Malware': 28, 'XSS': 29, 'Uploading_Attack': 30, 'SqlInjection': 31, 'CommandInjection': 32,              # Web
+                    'DictionaryBruteForce': 33}                                                                                                                 # Brute Force 
+
+# Some parts of the paper talk about 8 classes and split DDoS and DoS into seperate classes
+dict_8_classes = {  0: 0 ,                                                                                                                                      # Benign
+                    1:1, 2: 1, 3: 1, 4: 1, 5: 1, 6: 1, 7: 1, 8: 1, 9: 1, 10: 1, 11: 1, 12: 1,                                                                   # DDoS
+                    13: 7, 14: 7, 15: 7, 16: 7,                                                                                                                 # DoS                    
+                    17: 2, 18: 2, 19: 2,                                                                                                                        # Mirai
+                    20: 3, 21: 3, 22: 3, 23: 3, 24: 3,                                                                                                          # Reconnaissance
+                    25: 4, 26: 4,                                                                                                                               # Spoofing
+                    27: 5, 28: 5, 29: 5, 30: 5, 31: 5, 32: 5,                                                                                                   # Web
+                    33: 6}                                                                                                                                      # Brute Force
+
+# Example Notebook provides a "dict_7classes" list that is infact 8 classes. This shouldnt be used as far as I can tell                  
+dict_7_classes = {  0: 0 ,                                                                                                                                      # Benign
+                    1 :1, 2:1,  3:1, 4:1, 5:1, 6:1, 7:1, 8:1, 9:1, 10:1, 11:1, 12:1, 13:1, 14:1, 15:1, 16:1,                                                    # DDoS and DoS                    
+                    17: 2, 18: 2, 19: 2,                                                                                                                        # Mirai
+                    20: 3, 21: 3, 22: 3, 23: 3, 24: 3,                                                                                                          # Reconnaissance
+                    25: 4, 26: 4,                                                                                                                               # Spoofing
+                    27: 5, 28: 5, 29: 5, 30: 5, 31: 5, 32: 5,                                                                                                   # Web
+                    33: 6}                                                                                                                                      # Brute Force
+
+# Binary classes
+dict_2_classes = {  0: 0 ,                                                                                                                                      # Benign
+                    1 :1, 2:1,  3:1, 4:1, 5:1, 6:1, 7:1, 8:1, 9:1, 10:1, 11:1, 12:1, 13:1, 14:1, 15:1, 16:1,                                                    # DDoS and DoS  
+                    17: 1, 18: 1, 19: 1,                                                                                                                        # Mirai 
+                    20: 1, 21: 1, 22: 1, 23: 1, 24: 1,                                                                                                          # Reconnaissance
+                    25: 1, 26: 1,                                                                                                                               # Spoofing
+                    27: 1, 28: 1, 29: 1, 30: 1, 31: 1, 32: 1,                                                                                                   # Web
+                    33: 1}                                                                                                                                      # Brute Force
